5266b6f6fbb1ff0c0b13a497aaf1d9bf8e64981b
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2025-07-31 19:43:06 +0530

    Added solution for Matrix Diagnol Sum

---
+++ Matrix Diagnol Sum.java
@@ -0,0 +1,20 @@
+/*
+Leetcode:1572 - Matrix Diagnol Sum
+Leetcode link:https://leetcode.com/problems/matrix-diagonal-sum/
+*/
+
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum=0;
+        int n=mat.length;
+        for(int i=0;i<n;i++){
+            sum+=mat[i][i];
+            sum+=mat[n-1-i][i];
+        }
+        if(n%2!=0){
+            sum-=mat[n/2][n/2];
+        }
+        return sum;
+    }
+}

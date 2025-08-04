bd2866a2ed0726222a88bc52189b811c23c4fe0b
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2025-08-04 18:21:38 +0530

    Added solution Find the Highest Altitude.java

---
+++ Find the Highest Altitude.java
@@ -0,0 +1,19 @@
+/*
+Leetcode:1732 - Find the Highest Altitude
+Leetcode link:https://leetcode.com/problems/find-the-highest-altitude/
+*/
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int sum=0;
+        int max=0;
+        for(int i=0;i<gain.length;i++){
+            sum+=gain[i];
+            if(sum>max){
+                max=sum;
+            }
+            
+        }
+        return max;
+        
+    }
+}

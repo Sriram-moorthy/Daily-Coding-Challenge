e6318c24fb5eb4c4637cfd0fa93caed51fdffba7
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2025-09-13 18:49:00 +0530

    Added solution for To Lower Case

---
+++ To Lower Case.java
@@ -0,0 +1,12 @@
+/*
+ Leetcode: 709 - To Lower Case
+ Leetcode link:https://leetcode.com/problems/to-lower-case/
+*/
+
+class Solution {
+    public String toLowerCase(String s){
+        return s.toLowerCase();
+
+        
+    }
+}

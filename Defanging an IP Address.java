aae70e7448286e944e1ac41df1a5b3c99f3a1ac5
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2025-09-19 18:18:58 +0530

    Added solution for Defanging an IP Address

---
+++ Defanging an IP Address.java
@@ -0,0 +1,20 @@
+/*
+Leetcode: 1108 - Defanging an IP Address
+Leetcode link:https://leetcode.com/problems/defanging-an-ip-address/
+*/
+
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.'){
+                sb.append("[.]");
+                
+            }else{
+                sb.append(address.charAt(i));
+            }
+        }
+        return sb.toString();
+        
+    }
+}

f12e2e73b77a13af6656f8359c4730eeb4518fc1
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2025-10-30 14:47:58 +0530

    Added solution for Spiral Matrix

---
+++ Spiral Matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+       /*
+        Leetcode: 54 - Spiral Matrix
+        Leetcode link:https://leetcode.com/problems/spiral-matrix/
+      */
+        int left=0,right=col-1;
+        int top=0,bottom=row-1;
+        ArrayList<Integer> list=new ArrayList<>();
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                list.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                list.add(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                list.add(matrix[bottom][i]);
+                }
+            }
+            bottom--;
+            if(left<=right){
+                 for(int i=bottom;i>=top;i--){
+                list.add(matrix[i][left]);
+                }
+            }
+           left++;
+        }
+        return list;
+
+    }
+}

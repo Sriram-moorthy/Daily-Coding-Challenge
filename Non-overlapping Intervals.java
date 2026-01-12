4b651978cb274b10c1186e44d03b05884d1220f0
Sri Ram Moorthy <srirammoorthy11@gmail.com>
2026-01-12 12:16:18 +0530

    Implement solution for erasing overlapping intervals

---
+++ Non-overlapping Intervals.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals , new Comparator<int []>() {
+            public int compare(int[] a,int[] b){
+                return a[1]-b[1];
+            }
+        });
+        int remove=0;
+        int end=Integer.MIN_VALUE;
+        for(int[] interval:intervals){
+            if(interval[0]>=end){
+                end=interval[1];
+            }else{
+                remove++;
+            }
+        }
+        return remove;
+    }
+}
